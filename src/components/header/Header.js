ae36b248b1b647ac9c17ff3b675ae3935421ca2a
Tayyyyylor <bryan.h.pro@gmail.com>
2023-07-25 14:16:34 +0200

    deuxieme componetns

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Logo from "../logo/Logo"
+import Navbar from "../navbar/Navbar"
+import "./Header.css"
+
+function Header() {
+  return (
+    <header className="header-container">
+    <Logo/>
+    <Navbar/>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/logo/Logo.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import "./Logo.css"
+import { useState, useLayoutEffect, useRef } from "react"
+
+function Logo() {
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  const isMobile = window.innerWidth < 900;
+
+
+  const elementRef = useRef(null);
+  const handleVisibilityLogo = (visible) => {
+    setIsVisible(visible);
+  };
+
+  useLayoutEffect(() => {
+    if (isVisible) {
+      elementRef.current.classList.add("main-title-visible");
+      setIsVisible(false); 
+    }
+  }, [isVisible]);
+
+
+  return (
+    <>
+      {isMobile ? (
+        <a href="/">
+    <img className="img-logo" src="/TRANSPARENT.png" alt=""/>
+        </a>
+  ) : (
+            <div >
+              <a href="/" className="logo-container">
+                
+    <img className="img-logo" src="./TRANSPARENT.png" alt=""/>
+    <h1 ref={elementRef} className="main-title"> De <span className="color-letter-title">c</span>oeur à <span className="color-letter-title">c</span>oeur</h1>
+</a>
+            </div>
+  )}
+    
+    
+    </>
+  );
+}
+
+export default Logo
