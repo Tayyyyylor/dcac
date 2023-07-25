3277bfcef1bec567336a07637651c1ffb04abff8
Tayyyyylor <bryan.h.pro@gmail.com>
2023-07-25 14:55:03 +0200

    rezct router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import './App.css'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom'
+import Homepage from './pages/Homepage'
+import ErrorPage from './pages/ErrorPage'
 
 function App() {
+ 
   return (
-  <div></div>
-  );
+    <Routes>
+    <Route path='/' element={<Homepage/>}/>
+    <Route path='/error' element={<ErrorPage/>}/>
+
+    </Routes>
+  )
 }
 
-export default App;
+export default App
